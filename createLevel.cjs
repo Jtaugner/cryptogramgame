50d349d1522c7c4b56d334f68ba1a112302c2ed4
Jtaugner <rak123rak123@mail.ru>
2025-09-22 05:13:22 +0200

    new levels

--- createLevel.cjs
+++ createLevel.cjs
@@ -198,7 +198,7 @@ allLevels.forEach(level => {
 
 console.log(allNamesWithoutDescs);
 
-while(allLevels.length !== 1500){
+while(allLevels.length !== 1800){
      let index = Math.floor(Math.random() * allQuotes.length);
      let phrase = allQuotes[index];
      let text = phrase.text;
