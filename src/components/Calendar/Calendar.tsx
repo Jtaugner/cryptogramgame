2753b2b76ca3dd8a7a52aece697906c7378477d8
Jtaugner <rak123rak123@mail.ru>
2025-11-02 21:45:12 +0100

    change translatiin system

---
+++ src/components/Calendar/Calendar.tsx
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from 'react'
+import './Calendar.css'
+
+
+const Calendar: React.FC = () => {
+
+  useEffect(() => {
+
+
+    }, [])
+
+  return (
+    <div className="calendar">
+      
+    </div>
+  )
+}
+
+export default Calendar;
--- src/i18n.tsx
+++ src/i18n.tsx
@@ -2,16 +2,21 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
-import en from './locales/en/translation.json';
-import ru from './locales/ru/translation.json';
+import translations from './locales/translation.json';
+
+const resources: any = { en: { translation: {} }, ru: { translation: {} } };
+
+for (const key in translations) {
+  const entry = translations[key];
+  for (const lang in entry) {
+    resources[lang].translation[key] = entry[lang];
+  }
+}
 
 i18n
   .use(initReactI18next)
   .init({
-    resources: {
-      en: { translation: en },
-      ru: { translation: ru }
-    },
+    resources,
     fallbackLng: 'en',
     interpolation: { escapeValue: false }
   });
@@ -19,5 +24,22 @@ i18n
 export const changeLanguage = (language: string) => {
    i18n.changeLanguage(language);
 }
+
+/*
+let all = {}
+console.log(Object.keys(ru).length)
+console.log(Object.keys(en).length)
+let keys = Object.keys(ru);
+let keysEN = Object.keys(en);
+keys.forEach(k => {
+    if(!keysEN.includes(k)) console.log(k)
+    all[k] = {
+        "ru": ru[k],
+        "en": en[k]
+    }
+})
+console.log(all);
+localStorage.setItem("dasfvx", JSON.stringify(all))
+*/
 
 export default i18n;
